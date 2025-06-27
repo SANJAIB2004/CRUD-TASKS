caf388f648b37eb3c33d52df68977f9e3c3f175d
SANJAI B <123160135+SANJAIB2004@users.noreply.github.com>
2025-06-27 22:00:48 +0530

    Update settings.py

--- backend/task_manager/task_manager/settings.py
+++ backend/task_manager/task_manager/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'task_manager',
         'USER': 'postgres',
-        'PASSWORD': 'pranav',
+        'PASSWORD': 'your_password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
